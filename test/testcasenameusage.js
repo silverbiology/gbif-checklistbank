6d539fb09dfce1e51899cd27abc84cdf46400a79
sarahleddy <sarah@silverbiology.com>
2013-09-05 17:46:12 +0530

    file modified

--- test/testcasenameusage.js
+++ test/testcasenameusage.js
@@ -4,6 +4,6 @@ var path = require( 'path' );
 var gbifChecklistBank = require('../index');
 var clb = new gbifChecklistBank();
 console.log("chkng");
-clb.gbifName_usageAll({"sourceId":"119459225","language":"en"}, function( res ) {
+clb.gbifName_usageAll({"sourceId":"119459225","language":"en","datasetKey":"d7dddbf4-2cf0-4f39-9b2a-bb099caae36c"}, function( res ) {
 	console.log(res);
 	});
