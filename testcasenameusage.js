36a0fd4dabae022e76abb43b86fceb8c530dd63b
Michael Giddens <mikegiddens@silverbiology.com>
2013-09-09 15:55:36 -0600

    removed junk

--- testcasenameusage.js
+++
@@ -1,8 +0,0 @@
-var path = require( 'path' );
-//var needle = require('needle');
-
-var gbifChecklistBank = require('../index');
-var clb = new gbifChecklistBank();
-console.log("chkng");
-clb.gbifName_usageAll({}, function( res ) {
-	console.log(res);
